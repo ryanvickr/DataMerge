5c48b6992be21847f2ee04b69639204b6c0d321c
rvickram <rvickram@uwo.ca>
2018-03-26 20:38:44 -0400

    initial build
    basic gui created

--- src/datamerge/MainController.java
+++ src/datamerge/MainController.java
@@ -5,7 +5,12 @@
  */
 package datamerge;
 
+import java.io.File;
+import java.nio.file.Files;
 import java.net.URL;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +19,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 /**
  *
  * @author Ryan Vickramasinghe
@@ -22,9 +29,17 @@ public class MainController implements Initializable {
     
     @FXML
     private Button btn_findDirectory;
+    
+    @FXML
+    private Button btn_scan;
+    
     @FXML
     private TextField txt_directory;
     
+    /**
+     * this function will retrieve the user selected directory and display it
+     * @param event 
+     */
     @FXML
     private void handle_btn_findDirectory(ActionEvent event) {
         //Open directory selector
@@ -46,6 +61,39 @@ public class MainController implements Initializable {
           System.out.println("No Selection ");
         }
         
+    }
+    
+    /**
+     * This function will scan the directory for files to merge
+     * @param event 
+     */
+    @FXML
+    private void handle_btn_scan(ActionEvent event){
+        Path directoryPath = Paths.get(txt_directory.getText());
+        
+        //check to ensure directory is valid
+        if(!Files.exists(directoryPath, LinkOption.NOFOLLOW_LINKS) || directoryPath.toString().isEmpty()){
+            final JPanel panel = new JPanel();
+            JOptionPane.showMessageDialog(panel, "Could not find directory!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        else{ //if not valid, show error message:
+            scanFiles(directoryPath);
+        }
+    }
+    
+    /**
+     * this function will read files from a directory
+     * @param path the directory to scan files from
+     */
+    private void scanFiles(Path path){
+        System.out.println("Scanning...");
+        txt_directory.disableProperty().set(true);
+        btn_findDirectory.disableProperty().set(true);
+        
+        File directory = new File(path.toString());
+        for(File file : directory.listFiles()){
+            System.
+        }
     }
     
     @Override
